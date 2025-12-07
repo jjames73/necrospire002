61a8f714b8af51df7cf652b7c2f168d9376d41d6
jae_james <jjames73@uncc.edu>
2025-12-06 23:52:46 -0500

    Title

---
+++ main_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file()
+
+
+func _on_instructions_pressed() -> void:
+	get_tree().change_scene_to_file()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file()
